ed05b295fc324028a86f113ca6970109da826574
RogerJin <jinhanchenn@outlook.com>
2024-08-03 15:09:12 +0800

    improve PoolManager.java

--- src/main/java/org/superredrock/Kalium/parallelised/Pool/PoolManager.java
+++ src/main/java/org/superredrock/Kalium/parallelised/Pool/PoolManager.java
@@ -1,15 +1,16 @@
 package org.superredrock.Kalium.parallelised.Pool;
 
 import net.minecraft.world.level.Level;
-import org.superredrock.Kalium.Kalium;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.Closeable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.function.BiFunction;
 
 public class PoolManager implements Closeable {
-    private final ConcurrentHashMap<TickerPool, Level> ActivePool = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Level,List<TickerPool>> ActivePool = new ConcurrentHashMap<>();
     private final int maxThread;
     private int threadCount;
     private boolean closed = false;
@@ -33,41 +34,52 @@ public class PoolManager implements Closeable {
         }
     }
 
-    public BlockPool createBlockPool(Level level){
+
+    public <T extends TickerPool> T fork(int threads, Level level, @NotNull BiFunction<Integer,Level,T> builder){
+        //TODO: wait for improve
         if (this.closed){
             throw new IllegalStateException("The Manager has been closed");
         }
-        int allocateThread = this.allocate();
-        if (allocateThread <= 0){
-            allocateThread = 1;
-        }
-        BlockPool newBlockPool = new BlockPool(allocateThread,level);
-        this.register(newBlockPool,level);
-        Kalium.LOGGER.debug("New Block Pool [Threads:{}] has been created" ,allocateThread);
-        return newBlockPool;
-    }
-
-    public <T extends TickerPool> T fork(int threads, Level level, BiFunction<Integer,Level,T> builder){
-        //TODO: wait for improve
         T pool = builder.apply(threads,level);
         this.register(pool,level);
         return pool;
     }
 
+    public <T extends TickerPool> T fork(Level level, @NotNull BiFunction<Integer,Level,T> builder){
+        int allocateThread = this.allocate();
+        if (allocateThread <= 0){
+           allocateThread = 1;
+        }
+        return this.fork(allocateThread,level,builder);
+    }
+
+
+
     public void register(TickerPool pool,Level dimension){
-        ActivePool.put(pool,dimension);
+        this.ActivePool.putIfAbsent(dimension,new ArrayList<>());
+        List<TickerPool> insertList = this.ActivePool.get(dimension);
+        if (!insertList.contains(pool)){
+            insertList.add(pool);
+        }
     }
 
 
 
     @Override
     public void close() {
-        ActivePool.forEachKey(16,p ->p.isShutdown() ? null : p, ScheduledThreadPoolExecutor::shutdownNow);
-        ActivePool.clear();
+        this.ActivePool.forEachValue(Integer.MAX_VALUE,P -> P.isEmpty() ? null : P,
+                l ->{
+            for (TickerPool pool : l){
+                if (!pool.isShutdown()){
+                    pool.shutdownNow();
+                }
+            }
+        }
+        );
+        this.ActivePool.clear();
         this.closed = true;
 
     }
-
     public static final PoolManager mainPool = new PoolManager(32);
 
 }
